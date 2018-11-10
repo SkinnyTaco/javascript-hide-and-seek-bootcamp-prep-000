a2f690331f808f18611fcde058e14d3da5283a68
Daniel Dawson <dannyldawson@gmail.com>
2018-11-10 08:57:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let ranks = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + 1;
+  }
+}
+
+function deepestChild() {
+  let grandNode = document.querySelectorAll('#grand-node div');
+  let currentNode = grandNode;
+
+  return currentNode[grandNode.length - 1];
+}
